12122fb6633e63828ca53b810a351060363dedeb
songforthemuto <huni0423@naver.com>
2022-07-05 02:26:52 +0900

    setup default

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+import styled from "styled-components";
+
+const Container = styled.div`
+    display: flex;
+    width: 100vw;
+    height: 100vh;
+    background-color: ${(props) => props.theme.bgColor};
+`;
+const H1 = styled.h1`
+    color: ${(props) => props.theme.textColor};
+`;
+
+function App() {
+    return (
+        <Container>
+            <H1>Hello!</H1>
+        </Container>
+    );
+}
+
+export default App;
---
+++ src/theme.ts
@@ -0,0 +1,7 @@
+import { DefaultTheme } from "styled-components";
+
+export const theme: DefaultTheme = {
+    textColor: "#171717",
+    bgColor: "whitesmoke",
+    btnColor: "darkmagenta",
+};
