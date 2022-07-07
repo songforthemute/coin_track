88678c746f95f9aa64e65f1a014282485f142188
songforthemuto <huni0423@naver.com>
2022-07-08 08:21:56 +0900

    fetching coin history

--- src/api.ts
+++ src/api.ts
@@ -11,3 +11,12 @@ export const fetchCoinData = (coinId: string) => {
 export const fetchPriceData = (coinId: string) => {
     return fetch(`${BASE_URL}/tickers/${coinId}`).then((res) => res.json());
 };
+
+export const fetchCoinHistory = (coinId: string) => {
+    const endDate = Math.floor(Date.now() / 1000);
+    const startDate = endDate - 60 * 60 * 24 * 7;
+
+    return fetch(
+        `${process.env.REACT_APP_COIN_SERVER}/?coinId=${coinId}&start=${startDate}&end=${endDate}`
+    ).then((res) => res.json());
+};
