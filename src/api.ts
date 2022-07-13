cf3e10f0f299dd433eb83cc0c73e517cb28aec52
songforthemuto <huni0423@naver.com>
2022-07-13 19:20:37 +0900

    Update api.ts

--- src/api.ts
+++ src/api.ts
@@ -1,18 +1,19 @@
-const BASE_URL = process.env.REACT_APP_COINS_SERVER;
-
 export const fetchCoinsList = () => {
-    return fetch(`${BASE_URL}/coins`).then((res) => res.json());
+    return fetch(`${process.env.REACT_APP_COINS_SERVER}/coins`).then((res) =>
+        res.json()
+    );
 };
 
 export const fetchCoinData = (coinId: string) => {
-    return fetch(`${BASE_URL}/coins/${coinId}`).then((res) => res.json());
+    return fetch(`${process.env.REACT_APP_COINS_SERVER}/coins/${coinId}`).then(
+        (res) => res.json()
+    );
 };
 
 export const fetchPriceData = (coinId: string) => {
-    // `${BASE_URL}/tickers/${coinId}/?quotes=KRW,USD`
-    return fetch(`${BASE_URL}/tickers/${coinId}/?quotes=USD`).then((res) =>
-        res.json()
-    );
+    return fetch(
+        `${process.env.REACT_APP_COINS_SERVER}/tickers/${coinId}/?quotes=USD`
+    ).then((res) => res.json());
 };
 
 export const fetchCoinHistory = (coinId: string) => {
@@ -27,7 +28,7 @@ export const fetchCoinHistory = (coinId: string) => {
 };
 
 export const fetchCoinEvents = (coinId: string) => {
-    return fetch(`${BASE_URL}/coins/${coinId}/events`).then((res) =>
-        res.json()
-    );
+    return fetch(
+        `${process.env.REACT_APP_COINS_SERVER}/coins/${coinId}/events`
+    ).then((res) => res.json());
 };
